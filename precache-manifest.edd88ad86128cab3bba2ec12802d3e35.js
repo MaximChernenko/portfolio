cb1dc747ba6452008c25c529064a914100e1d4c3
Maxim Chernenko <twiggyramirez111@gmail.com>
2019-06-22 13:41:18 +0300

    Updates

---
+++ precache-manifest.edd88ad86128cab3bba2ec12802d3e35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c9814365a244d21a742fc0c75c39da",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3cb7ab4fd0ffa06b7fa8",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "59af8a29be2295daa856",
+    "url": "/portfolio/static/css/main.50069fc3.chunk.css"
+  },
+  {
+    "revision": "3cb7ab4fd0ffa06b7fa8",
+    "url": "/portfolio/static/js/2.7e76a1a7.chunk.js"
+  },
+  {
+    "revision": "59af8a29be2295daa856",
+    "url": "/portfolio/static/js/main.ab6a4c10.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "91f6d686d6bf8a032ef95cab76e9366b",
+    "url": "/portfolio/static/media/photo.91f6d686.jpg"
+  },
+  {
+    "revision": "aa36000276914761c6e3a22a0c07b231",
+    "url": "/portfolio/static/media/sprite.aa360002.svg"
+  }
+]);
